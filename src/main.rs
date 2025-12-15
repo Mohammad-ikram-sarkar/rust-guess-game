0e6e54d53177039a064d2d317112c197230504a4
Mohammad-ikram-sarkar <islamikram357@gmail.com>
2025-12-15 13:29:18 +0600

    random number

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 
 use std::io;
+use rand::Rng;
 
 fn main () {
     println!("Guess the number !");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("this secret_number is : {secret_number}");
     println!("give me input");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess)
